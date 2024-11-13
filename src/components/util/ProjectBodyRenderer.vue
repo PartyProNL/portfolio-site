ebbc6d971afa7a1d903a3e636f9db5b263d4c519
Youri Scheepers <youri.scheepers@hva.nl>
2024-11-13 10:06:37 +0100

    Create component to render all body parts to components

---
+++ src/components/util/ProjectBodyRenderer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <component
+        v-for="(part, index) in parts"
+        :key="index"
+        :is="components[part.render().component]"
+        v-bind="part.render().props"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from "vue";
+import {ProjectBodyPart} from "../models/ProjectBodyPart.ts";
+import BodyTextRenderer from "./BodyTextRenderer.vue";
+import BodyImageRenderer from "./BodyImageRenderer.vue";
+
+defineProps<{
+  parts: ProjectBodyPart[];
+}>();
+
+const components = {
+  BodyTextRenderer,
+  BodyImageRenderer,
+};
+</script>
