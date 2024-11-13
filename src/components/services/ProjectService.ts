ec00c5ff066531efa072b8ecb0ff360742046dce
Youri Scheepers <youri.scheepers@hva.nl>
2024-11-13 18:02:14 +0100

    Add more project data

--- src/components/services/ProjectService.ts
+++ src/components/services/ProjectService.ts
@@ -40,6 +40,28 @@ export class ProjectService {
             new ProjectBodyImage("./img/elections/elections-3.png", ""),
             new ProjectBodyImage("./img/elections/elections-4.png", ""),
         ]
+    },
+    {
+        name: "BandiThemepark",
+        slogan: "Add slogan!",
+        image: "./img/banner-4.png",
+        backgroundColor: "#fcf8e8",
+        textColor: "#280003",
+        body: [
+            new ProjectBodyText("Back in february 2020 I got invited to join a project called BandiThemepark. BandiThemepark is a fictional themepark, built in a modified version of the famous videogame Minecraft. I joined this project as a developer, but quickly also became the co-owner."),
+            new ProjectBodyText("I have been working on the project as a volunteer developer since then. My main task is developing the Kotlin back-end that runs the themepark part, but I also work on some of our other services, such as our websites and internal tools."),
+            new ProjectBodyImage("./img/bandi/landing.png", "The landing page of our main site, www.bandithemepark.net"),
+            new ProjectBodyText("Our main website is powered by a content management system (CMS) called Strapi, so that anyone in our team can easily edit and update the site. It is also Search Engine Optimized."),
+            new ProjectBodyText("BandiThemepark exists out of a lot of different services. This includes our main game-server, a website, a webstore and internal administration and management tools. Almost all of these run using Docker. We also have communication between most of our services, some via a (partially public) REST API, and some using MQTT."),
+            new ProjectBodyImage("./img/bandi/parkops-1.png", "The login page of our administration tool"),
+            new ProjectBodyImage("./img/bandi/parkops-2.png", "The screen to manage so-called achievements players can get by exploring the themepark"),
+            new ProjectBodyText("We also have a (not currently public) store, where people can buy optional cosmetics and other features to add to their experience."),
+            new ProjectBodyImage("./img/bandi/store-1.png", "The home page of our store"),
+            new ProjectBodyImage("./img/bandi/store-2.png", "The shopping cart sheet that appears before checkout"),
+            new ProjectBodyImage("./img/bandi/store-3.png", "Further details on the product page of our 'VIP-rank'. All renders and art are by me as well"),
+            new ProjectBodyText("These are some of the web-apps I have worked on for BandiThemepark. I was also involved in developing the main REST API that powers most of our services, but my main task was the game-server(s) code. Most of that is hard to show, especially if you are not familiar with Minecraft. I do have some visualization for the rollercoasters I programmed though."),
+            new ProjectBodyText("This picture shows our rollercoaster system. Minecraft does not have (realistic) rollercoasters as part of the game, so I had to program my own. At their core, these rollercoasters are Bezier splines, that have trains that follow these splines. These trains have simulated physics (written by me), and their rendering is powered by matrices and quaternions. Each rollercoaster also has a realistic control system, modeled after real-life examples. There can also be collisions between trains.")
+        ]
     }]
 
     public getProjects(): Project[] {
