0fbd5bdcd358b44fe64a1430e73bf71309410f00
Youri Scheepers <youri.scheepers@hva.nl>
2024-11-13 10:24:25 +0100

    Update image paths so that they don't break with new base URL

--- src/components/services/ProjectService.ts
+++ src/components/services/ProjectService.ts
@@ -7,18 +7,18 @@ export class ProjectService {
     {
         name: "Dokkie",
         slogan: "Split the bill, not your friend group.",
-        image: "/img/banner-1.jpg",
+        image: "./img/banner-1.jpg",
         backgroundColor: "#fcf8e8",
         textColor: "#280003",
         body: [
             new ProjectBodyText("Not many things are as deeply rooted into Dutch culture as sharing the bill. Many people nowadays use apps to help this process, to keep track of all expenses, and to accurately calculate what everyone has to pay. Dokkie does exactly that. It was my first project for my major in Computer Science."),
-            new ProjectBodyImage("/img/banner-2.jpg", "My text")
+            new ProjectBodyImage("./img/dokkie/dokkie-1.png", "My text")
         ]
     },
     {
         name: "Code Exchange",
         slogan: "Empowering students to help each other.",
-        image: "/img/banner-2.jpg",
+        image: "./img/banner-2.jpg",
         backgroundColor: "#D7FDF0",
         textColor: "#171738",
         body: [
@@ -28,17 +28,17 @@ export class ProjectService {
     {
         name: "Elections",
         slogan: "Accessible insight into election data for everyone.",
-        image: "/img/banner-3.jpg",
+        image: "./img/banner-3.jpg",
         backgroundColor: "#fcf8e8",
         textColor: "#280003",
         body: [
             new ProjectBodyText("For my third semester, I was tasked with creating a website together with my team to view Dutch election results. We built a front-end using TypeScript, Vue3 and TailwindCSS, and connected it to a Java Spring Boot with Hibernate back-end."),
-            new ProjectBodyImage("/img/elections/elections-1.png", ""),
+            new ProjectBodyImage("./img/elections/elections-1.png", ""),
             new ProjectBodyText("The main task in this project was loading the election data. We used publicly available XML-files supplied by the government, and had to parse these to put them into an SQL database. These datasets are quite big, around 2-3GB each. We had limited computing resources, so our code being optimized was very important."),
             new ProjectBodyText("I did many optimizations, but some included optimizing the indexes of our database, utilizing multithreading and using Java's PreparedStatements."),
-            new ProjectBodyImage("/img/elections/elections-2.png", ""),
-            new ProjectBodyImage("/img/elections/elections-3.png", ""),
-            new ProjectBodyImage("/img/elections/elections-4.png", ""),
+            new ProjectBodyImage("./img/elections/elections-2.png", ""),
+            new ProjectBodyImage("./img/elections/elections-3.png", ""),
+            new ProjectBodyImage("./img/elections/elections-4.png", ""),
         ]
     }]
 
