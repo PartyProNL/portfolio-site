f8d0aaaf29ef02411b482b8ca1fea1de08870e20
Youri Scheepers <youri.scheepers@hva.nl>
2024-11-13 23:56:38 +0100

    Add about page

---
+++ src/components/util/CascadingBackground.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="grid grid-cols-4 grid-rows-4 w-full h-full absolute top-0 left-0">
+    <div v-for="i in 16" class="w-full h-full bg-black origin-center">
+      <div :class="{ revert: reverse }" class="bg-white h-full w-0 group-hover:w-full transition-all" :style="{transitionDelay: getTransitionDelay(i)+'ms'}"></div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  reverse: Boolean
+})
+
+const getTransitionDelay = (i: number): number => {
+  if(props.reverse) {
+    return (16-i)*10
+  } else {
+    return i*10
+  }
+}
+</script>
+
+<style scoped>
+.revert {
+  margin-left: auto;
+}
+</style>
