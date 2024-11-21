faf7c624ef4300c6e12eb6c25e342544d2f8fbc1
Youri Scheepers <youri.scheepers@hva.nl>
2024-11-21 19:24:51 +0100

    Add new projects page

---
+++ src/components/pages/Projects.vue
@@ -0,0 +1,79 @@
+<template>
+  <NavigationBar active-page="projects"/>
+
+  <div class="w-full h-screen bg-white flex items-center justify-center overflow-hidden" @wheel="onScroll">
+    <div ref="trackElement" class="flex gap-4 track" :style="{transform:'translateX(calc('+-trackPosition+'px + 50% - '+trackOffset+'px))'}">
+      <div
+          v-for="(project, index) in projects"
+          @click="expandedProject = index"
+          :class="{'expanded-project-card': index == expandedProject}"
+          class="bg-cover project-card translate-x-[0%] relative"
+          :style="{
+            backgroundImage: `url(${project.image})`,
+            backgroundPosition: `${trackPercentage*100}% center`,
+            filter: `grayscale(${expandedProject == index ? 0 : 100}%)`
+          }"
+      >
+        <p v-if="expandedProject == index" class="absolute w-[600px] font-[600] text-[64px] top-0 left-0 -translate-x-[3px] -translate-y-[80px]">{{ project.name }}</p>
+      </div>
+    </div>
+
+    <div class="absolute bottom-0 left-1/2 -translate-x-1/2 p-4">3 / 5</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {ProjectService} from "../services/ProjectService.ts";
+import {computed, ref} from "vue";
+import NavigationBar from "../NavigationBar.vue";
+
+const projectService = new ProjectService()
+const projects = ref(projectService.getProjects())
+
+const trackElement = ref()
+const trackPosition = ref(0)
+const trackPercentage = ref(0)
+function onScroll(event: WheelEvent) {
+  expandedProject.value = -1
+  trackPosition.value += event.deltaY / 2
+
+  if(trackPosition.value < 0) {
+    trackPosition.value = 0;
+  }
+
+  const trackWidth = trackElement.value.clientWidth-280;
+  if(trackPosition.value > trackWidth) {
+    trackPosition.value = trackWidth
+  }
+
+  trackPercentage.value = trackPosition.value / trackWidth
+}
+
+const trackOffset = computed(() => {
+  if(expandedProject.value == -1) {
+    return 140
+  } else {
+    return 300
+  }
+})
+
+const expandedProject = ref(-1)
+</script>
+
+<style scoped>
+.track {
+  transition: transform 0.7s cubic-bezier(0,.35,.44,.98);
+}
+
+.project-card {
+  height: 420px;
+  width: 280px;
+  transition: background-position 0.7s cubic-bezier(0,.35,.44,.98),
+              width 0.7s cubic-bezier(0,.35,.44,.98),
+              filter 0.3s ease-in-out;
+}
+
+.expanded-project-card {
+  width: 600px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,11 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import {createRouter, createWebHashHistory} from "vue-router";
-import HomePage from "./components/HomePage.vue";
-import ProjectPage from "./components/ProjectPage.vue";
-import AboutPage from "./components/AboutPage.vue";
 import {createHead} from "@unhead/vue";
-import AboutPersonalPage from "./components/AboutPersonalPage.vue";
-import AboutProfessionalPage from "./components/AboutProfessionalPage.vue";
-import MobilePage from "./components/MobilePage.vue";
+import Projects from "./components/pages/Projects.vue";
 
 const routes = [
-    { path: '/', component: HomePage },
-    { path: '/about', component: AboutPage },
-    { path: '/about/personal', component: AboutPersonalPage },
-    { path: '/about/professional', component: AboutProfessionalPage },
-    { path: '/mobile', component: MobilePage },
-    { path: '/project/:id', component: ProjectPage },
+    { path: '/', component: Projects }
 ]
 
 const router = createRouter({
