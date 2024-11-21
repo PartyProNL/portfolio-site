f3fe07b4a809185a11b4c2efd966d84dd42b9de9
Youri Scheepers <youri.scheepers@hva.nl>
2024-11-21 19:23:22 +0100

    Remove custom mouse

--- src/App.vue
+++ src/App.vue
@@ -1,94 +1,11 @@
 <template>
-  <RouterView class="z-0" @mousemove="updateMouse"/>
-
-  <div ref="mouseElement" class="size-16 top-0 left-0 absolute pointer-events-none text-white z-10">
-    <div ref="mouseInner" class="size-16 bg-[#E84855] rounded-full flex justify-center items-center mouse-scale scale-[.25]">
-      <svg v-if="activeIcon == 'left'" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
-      </svg>
-
-      <svg v-if="activeIcon == 'banner'" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-      </svg>
-
-      <svg v-if="activeIcon == 'right'" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
-      </svg>
-
-      <svg v-if="activeIcon == 'professional'" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-        <path d="M11.7 2.805a.75.75 0 0 1 .6 0A60.65 60.65 0 0 1 22.83 8.72a.75.75 0 0 1-.231 1.337 49.948 49.948 0 0 0-9.902 3.912l-.003.002c-.114.06-.227.119-.34.18a.75.75 0 0 1-.707 0A50.88 50.88 0 0 0 7.5 12.173v-.224c0-.131.067-.248.172-.311a54.615 54.615 0 0 1 4.653-2.52.75.75 0 0 0-.65-1.352 56.123 56.123 0 0 0-4.78 2.589 1.858 1.858 0 0 0-.859 1.228 49.803 49.803 0 0 0-4.634-1.527.75.75 0 0 1-.231-1.337A60.653 60.653 0 0 1 11.7 2.805Z" />
-        <path d="M13.06 15.473a48.45 48.45 0 0 1 7.666-3.282c.134 1.414.22 2.843.255 4.284a.75.75 0 0 1-.46.711 47.87 47.87 0 0 0-8.105 4.342.75.75 0 0 1-.832 0 47.87 47.87 0 0 0-8.104-4.342.75.75 0 0 1-.461-.71c.035-1.442.121-2.87.255-4.286.921.304 1.83.634 2.726.99v1.27a1.5 1.5 0 0 0-.14 2.508c-.09.38-.222.753-.397 1.11.452.213.901.434 1.346.66a6.727 6.727 0 0 0 .551-1.607 1.5 1.5 0 0 0 .14-2.67v-.645a48.549 48.549 0 0 1 3.44 1.667 2.25 2.25 0 0 0 2.12 0Z" />
-        <path d="M4.462 19.462c.42-.419.753-.89 1-1.395.453.214.902.435 1.347.662a6.742 6.742 0 0 1-1.286 1.794.75.75 0 0 1-1.06-1.06Z" />
-      </svg>
-
-      <svg v-if="activeIcon == 'personal'" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-        <path fill-rule="evenodd" d="M9 4.5a.75.75 0 0 1 .721.544l.813 2.846a3.75 3.75 0 0 0 2.576 2.576l2.846.813a.75.75 0 0 1 0 1.442l-2.846.813a3.75 3.75 0 0 0-2.576 2.576l-.813 2.846a.75.75 0 0 1-1.442 0l-.813-2.846a3.75 3.75 0 0 0-2.576-2.576l-2.846-.813a.75.75 0 0 1 0-1.442l2.846-.813A3.75 3.75 0 0 0 7.466 7.89l.813-2.846A.75.75 0 0 1 9 4.5ZM18 1.5a.75.75 0 0 1 .728.568l.258 1.036c.236.94.97 1.674 1.91 1.91l1.036.258a.75.75 0 0 1 0 1.456l-1.036.258c-.94.236-1.674.97-1.91 1.91l-.258 1.036a.75.75 0 0 1-1.456 0l-.258-1.036a2.625 2.625 0 0 0-1.91-1.91l-1.036-.258a.75.75 0 0 1 0-1.456l1.036-.258a2.625 2.625 0 0 0 1.91-1.91l.258-1.036A.75.75 0 0 1 18 1.5ZM16.5 15a.75.75 0 0 1 .712.513l.394 1.183c.15.447.5.799.948.948l1.183.395a.75.75 0 0 1 0 1.422l-1.183.395c-.447.15-.799.5-.948.948l-.395 1.183a.75.75 0 0 1-1.422 0l-.395-1.183a1.5 1.5 0 0 0-.948-.948l-1.183-.395a.75.75 0 0 1 0-1.422l1.183-.395c.447-.15.799-.5.948-.948l.395-1.183A.75.75 0 0 1 16.5 15Z" clip-rule="evenodd" />
-      </svg>
-
-      <svg v-if="activeIcon == 'expand'" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6 scale-125">
-        <path fill-rule="evenodd" d="M11.47 4.72a.75.75 0 0 1 1.06 0l3.75 3.75a.75.75 0 0 1-1.06 1.06L12 6.31 8.78 9.53a.75.75 0 0 1-1.06-1.06l3.75-3.75Zm-3.75 9.75a.75.75 0 0 1 1.06 0L12 17.69l3.22-3.22a.75.75 0 1 1 1.06 1.06l-3.75 3.75a.75.75 0 0 1-1.06 0l-3.75-3.75a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
-      </svg>
-    </div>
-  </div>
+  <RouterView/>
 </template>
 
 <script setup lang="ts">
-import {onBeforeUnmount, onMounted, provide, ref} from "vue";
-import {mobileCheck} from "./components/util/DetectMobile.ts";
-import {useRouter} from "vue-router";
 
-const mouseElement = ref<HTMLElement>()
-const mouseInner = ref<HTMLElement>()
-const activeIcon = ref('')
-
-let lastY = 0;
-let lastX = 0;
-const updateMouse = (event: MouseEvent) => {
-  const scrolledHeight = window.scrollY
-  lastX = event.x-32
-  lastY = event.y-32
-  mouseElement.value!.style.transform = `translate(${lastX}px, ${lastY+scrolledHeight}px)`
-}
-
-const updateMouseOnScroll = () => {
-  const scrolledHeight = window.scrollY
-  mouseElement.value!.style.transform = `translate(${lastX}px, ${lastY+scrolledHeight}px)`
-}
-
-const updateMouseSize = () => {
-  let scale = 1;
-  if(activeIcon.value == '') scale = 0.25;
-
-  mouseInner.value!.style.transform = `scale(${scale})`
-}
-
-const setIcon = (value: string) => {
-  activeIcon.value = value;
-  updateMouseSize()
-}
-
-onMounted(() => {
-  window.addEventListener("scroll", updateMouseOnScroll);
-  if(onMobile) mouseElement.value!.classList.add("hidden");
-})
-
-onBeforeUnmount(() => {
-  window.addEventListener("scroll", updateMouseOnScroll);
-})
-
-provide("setIcon", setIcon)
-
-const router = useRouter()
-let onMobile = false
-if(mobileCheck()) {
-  router.push("/mobile")
-  onMobile = true
-}
 </script>
 
 <style scoped>
-.mouse-scale {
-  transform-origin: center;
-  transition: transform 0.2s ease-in-out;
-}
+
 </style>
