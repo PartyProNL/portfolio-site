3e109ae730ff438b901b262bafe1fb3dc0ca212f
Youri Scheepers <youri.scheepers@hva.nl>
2024-11-21 19:22:44 +0100

    Create unused intro animation

---
+++ src/components/animation/Intro.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="w-full min-h-screen bg-white flex items-center justify-center">
+    <div class="overflow-hidden flex gap-1 font-[600] text-black text-2xl">
+      <p class="text -translate-y-full">Youri</p>
+      <p class="text -translate-y-full" :style="{animationDelay: '100ms'}">Scheepers</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.text {
+  animation: text;
+  animation-duration: 2s;
+  animation-fill-mode: forwards;
+  animation-timing-function: cubic-bezier(0,.9,1,.1);
+}
+
+@keyframes text {
+  0% {
+    transform: translateY(100%);
+  }
+
+  90% {
+    opacity: 1;
+  }
+
+  100% {
+    transform: translateY(-100%);
+    opacity: 0;
+  }
+}
+</style>
