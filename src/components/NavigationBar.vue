5fb9a5913d926ea2c6be50fdebcdac56c583dcc1
Youri Scheepers <youri.scheepers@hva.nl>
2024-11-23 16:56:44 +0100

    Improve responsiveness of navigation bar

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="fixed top-0 left-0 w-full flex items-center justify-between py-4 px-8 text-[16px]">
-    <div class="font-[600]">Youri Scheepers</div>
+    <div class="font-[600] hidden md:block">Youri Scheepers</div>
+    <div class="font-[600] block md:hidden">YS</div>
 
-    <div class="absolute left-1/2 flex -translate-x-1/2 gap-16 font-[500]">
+    <div class="absolute left-1/2 flex -translate-x-1/2 gap-4 md:gap-16 font-[500]">
       <p v-if="activePage == 'projects'" class="text-black cursor-pointer">Projects</p>
       <p v-else @click="openProjects" class="text-[#666666] cursor-pointer">Projects</p>
 
