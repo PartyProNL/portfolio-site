675b0075f254d4891f896214821b5fdf53f2232d
Youri Scheepers <youri.scheepers@hva.nl>
2024-11-28 00:02:04 +0100

    Change scroll behaviour to scroll back up when switching pages

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,10 @@ const routes = [
 const router = createRouter({
     history: createWebHashHistory(),
     routes,
+    scrollBehavior(_, __, savedPosition) {
+        if (savedPosition) return savedPosition;
+        return { top: 0 };
+    }
 })
 
 const head = createHead()
